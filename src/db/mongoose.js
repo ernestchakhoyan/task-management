eae9fa833b4b5454478f2973f2303a1a96a5ae10
ernest <echakhoyan@bluip.com>
2020-04-01 01:44:42 +0400

    Read all  tasks and get by id

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,30 +4,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useNewUrlParser: true,
     useCreateIndex: true
 });
-
-const Task = mongoose.model("Task", {
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    completed: {
-        type: Boolean,
-        required: false,
-        default: false
-    }
-});
-
-const myTask = new Task({
-    description: "Challenge 2",
-    completed: true
-});
-
-myTask.save()
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
