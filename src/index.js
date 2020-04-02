b3cebbe5a74e9fbd10809274bb0cc3643908f7b9
ernest <echakhoyan@bluip.com>
2020-04-02 11:59:00 +0400

    Refactoring routes

--- src/index.js
+++ src/index.js
@@ -1,150 +1,15 @@
 const express = require("express");
 require("./db/mongoose");
-
-const User = require("./models/user");
-const Task = require("./models/task");
+const userRouter = require("./routes/users");
+const taskRouter = require("./routes/tasks");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
-app.post("/users", async (req, res) => {
-    try {
-        const user = new User(req.body);
-        await user.save();
-        res.status(201).send(user);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-app.get("/users", async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).send(users);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.get("/users/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.patch("/users/:id", async (req, res) => {
-    const _id = req.params.id;
-    const updates = Object.keys(req.body);
-    const allowedUpdates = [ "name", "email", "password", "age" ];
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: "Invalid updates" });
-    }
-
-    try {
-        const user = await User.findOneAndUpdate(_id, req.body, { new: true, runValidators: true });
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.status(200).send(user);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.delete("/users/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findByIdAndDelete(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.post("/tasks", async (req, res) => {
-    try {
-        const task = new Task(req.body);
-        await task.save();
-        res.status(201).send(task);
-    } catch (e) {
-        res.status(400);
-        res.send(e);
-    }
-});
-
-app.get("/tasks", async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.send(tasks);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const task = await Task.findById(_id);
-        if (!task) {
-            return res.status(404).send();
-        }
-        res.send(task);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-    const _id = req.params.id;
-    const allowedUpdates = [ "description", "completed" ];
-    const updates = Object.keys(req.body);
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(404).send({ error: "Invalid task updates" });
-    }
-
-    try {
-        const task = await Task.findOneAndUpdate(_id, req.body, { new: true, runValidators: true });
-        if (!task) {
-            return res.status(404).send();
-        }
-        res.status(200).send(task);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const task = await Task.findByIdAndDelete(_id);
-        if (!task) {
-            return res.status(404).send();
-        }
-        res.send(task);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
 
 app.listen(port, () => {
     console.log("Server is up on port " + 3000);
