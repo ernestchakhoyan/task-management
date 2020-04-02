dbacdf588194f05cb0ed5f5e1896341cf11fa132
ernest <echakhoyan@bluip.com>
2020-04-02 22:56:06 +0400

    Schema with password

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -47,7 +47,11 @@ router.patch("/tasks/:id", async (req, res) => {
     }
 
     try {
-        const task = await Task.findOneAndUpdate(_id, req.body, { new: true, runValidators: true });
+        const task = await Task.findById(_id);
+
+        updates.forEach((update) => task[update] = req.body[update]);
+        task.save();
+
         if (!task) {
             return res.status(404).send();
         }
