e2373a4ac6decb0b98b13053a72d6e92bc63936d
ernest <echakhoyan@bluip.com>
2020-04-03 19:35:08 +0400

    Sign up return access token

--- src/routes/users.js
+++ src/routes/users.js
@@ -16,8 +16,8 @@ router.post("/users/login", async (req, res) => {
 router.post("/users", async (req, res) => {
     try {
         const user = new User(req.body);
-        await user.save();
-        res.status(201).send(user);
+        const token = user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
